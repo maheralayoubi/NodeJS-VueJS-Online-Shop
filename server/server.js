10e1c66da25e0b48b297c65905aa1c007c4ae248
Maher <maher91syr@gmail.com>
2021-02-16 21:47:15 +0900

    using POST to send data from front-end to back-end

--- server/server.js
+++ server/server.js
@@ -12,8 +12,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// GET - Retrieve data from the server
 app.get('/', (req, res) => {
     res.json('Hello There!');
+});
+
+// POST - Send data from front-end to back-end
+app.post('/', (req, res) => {
+    console.log(req.body.name);
 });
 
 app.listen(3000, (err) => {
