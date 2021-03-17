6c2eee84ff316882008fc4790fda5c4cc125d037
Maher <maher91syr@gmail.com>
2021-03-17 23:55:19 +0900

    Product updated

--- server/models/product.js
+++ server/models/product.js
@@ -1,2 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const ProductSchema = new Schema({
+    category: { type: Schema.Types.ObjectId, ref: 'Category'},
+    owner: { type: Schema.Types.ObjectId, ref: 'Owner'},
+    title: String,
+    description: String,
+    photo: String,
+    price: Number,
+    stockQuantity: Number,
+    rating: [Number]
+});
+
+// Exporting our userSchema so we can use it in another file
+module.exports = mongoose.model('Product', ProductSchema);
 
