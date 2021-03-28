9f25f31188f08704d6b2395e6bbf633fc1e2d7a8
Maher <maher91syr@gmail.com>
2021-03-28 16:16:04 +0900

    Creating POST method in routers

---
+++ server/routes/product.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const Product = require('../models/product');
+//
+// title: String,
+//     description: String,
+//     photo: String,
+//     price: Number,
+//     stockQuantity: Number,
+//     rating: [Number]
+
+// POST request for creating a new product
+router.post('/products', async (req, res) => {
+    try {
+        let product = new Product();
+        product.title = req.body.title;
+        product.description = req.body.description;
+        product.photo = req.body.photo;
+        product.price = req.body.price;
+        product.stockQuantity = req.body.stockQuantity;
+
+        await product.save();
+
+        res.json({
+            status: true;
+            massage: 'Successfully saved';
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false;
+            message: err.message;
+        })
+    }
+})
+
+// GET request for getting all products
+
+
+// GET request for getting a single product
+
+
+// PUT request for updating a single product
+
+
+// DELETE request for deleting a single product
