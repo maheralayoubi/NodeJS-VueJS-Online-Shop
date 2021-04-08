ecfce9f32a178ddbf33edb571cab62b9dbf6c40f
Maher <maher91syr@gmail.com>
2021-04-08 23:13:28 +0900

    product router removed

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,27 +2,7 @@ const router = require('express').Router();
 const Product = require('../models/product');
 
 // POST request for creating a new product
-router.post('/products', async (req, res) => {
-    try {
-        let product = new Product();
-        product.title = req.body.title;
-        product.description = req.body.description;
-        product.photo = req.body.photo;
-        product.stockQuantity = req.body.stockQuantity;
 
-        await product.save();
-
-        res.json({
-            status: true,
-            massage: 'Successfully saved'
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        })
-    }
-});
 
 // GET request for getting all products
 
