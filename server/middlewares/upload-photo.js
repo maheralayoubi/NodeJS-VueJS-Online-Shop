ca6dd948cb78a928b572a91302cecb908af4d2cd
Maher <maher91syr@gmail.com>
2021-04-23 15:04:55 +0900

    Creating a function to connecting to AWS s3 storage

--- server/middlewares/upload-photo.js
+++ server/middlewares/upload-photo.js
@@ -3,3 +3,31 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 // Multer-s3 is a library that upload the photo directly to AWS s3 bucket
 const multerS3 = require('multer-s3');
+
+// Access AWS S3 storage
+aws.config.update({
+    secretAccessKey: process.env.AWSAccessKeyId,
+    accessKeyId: process.env.AWSSecretKey
+})
+
+const s3 = new aws.s3();
+
+// Upload image function
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'shop-clone-v1',
+        acl: 'public-read',
+        metadata: (req, file, cb) => {
+            cb(null, {fieldName: file.fieldName});
+        },
+        key: (req, file, cb) => {
+            cb(null, Date.now().toString())
+        }
+    })
+})
+
+module.exports = upload;
+
+
